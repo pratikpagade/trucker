7599925090014700352a9957be3e0542041ddf74
Pratik Pagade <pratik.pagade@sjsu.edu>
2020-07-02 00:15:19 -0700

    Update signup.js
    added some functionality:OTHER DEVELOPER

--- src/signup/signup.js
+++ src/signup/signup.js
@@ -1,1 +1,2 @@
-console.log(These are MY changes or lines of code)
+console.log(These are MY changes or lines of code .. also modifying this line:other dev).
+console.log(These are OTHER DEVELOPER's changes).
